ff137062d84d188fd9204b760836e6119916ddef
Zaboronek-EXTERNAL Jakub <10054889@gemalto.com>
2019-10-21 08:36:22 +0200

    Added new functional method

--- src/main/java/pl/udemy/FP01Functional.java
+++ src/main/java/pl/udemy/FP01Functional.java
@@ -7,11 +7,13 @@ public class FP01Functional {
 
     public static void main(String[] args) {
 
-        printAllNumbersInListFunctional(List.of(12, 9, 4, 64, 32));
+//        printAllNumbersInListFunctional(List.of(12, 9, 4, 64, 32));
+//        printAllEvenNumbersInListFunctional(List.of(12,12,44,322,1,2,333,78));
+        printAllCoursesContaining4chars(List.of("Spring","Spring boot","API","Microservices","AWS","PCF","Azure","Docker"));
     }
 
     private static boolean isEven(Integer k) {
-        return k%2==0;
+        return k % 2 == 0;
     }
 
 
@@ -19,4 +21,27 @@ public class FP01Functional {
         numbers.stream()
                 .filter(FP01Functional::isEven)
                 .forEach(System.out::println);
-        }}
+    }
+
+    private static void printAllEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(number->number%2==0)
+                .forEach(System.out::println);
+    }
+
+    private static void printAllCourses(List<String> courses) {
+        courses.stream()
+                .forEach(System.out::println);
+    }
+    private static void printAllCoursesContainingSpring(List<String> courses) {
+        courses.stream()
+                .filter(course->course.contains("Spring"))
+                .forEach(System.out::println);
+    }
+    private static void printAllCoursesContaining4chars(List<String> courses) {
+        courses.stream()
+                .filter(course->course.length()>4)
+                .forEach(System.out::println);
+    }
+
+}
